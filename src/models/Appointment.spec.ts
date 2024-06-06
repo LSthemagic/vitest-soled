daf6ce3e53f8a9387505a66715f175976b0e947f
Railan Santana <lansilvah14fsa@gmail.com>
2024-06-06 10:02:00 -0300

    feat: test cannot create appointment with end Date before of start date

--- src/models/Appointment.spec.ts
+++ src/models/Appointment.spec.ts
@@ -2,13 +2,31 @@ import { expect, test } from "vitest";
 import { Appointment } from "./Appointment";
 
 test("Create an appointment", () => {
+  const startsAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() + 1);
   const appointment = new Appointment({
     costumer: "Jon Doe",
-    startsAt: new Date(),
-    endsAt: new Date(),
+    startsAt,
+    endsAt,
   });
 
-  expect(appointment).toBeInstanceOf(Appointment)
-  expect(appointment.costumer).toEqual('Jon Doe')
+  expect(appointment).toBeInstanceOf(Appointment);
+  expect(appointment.costumer).toEqual("Jon Doe");
+});
 
+test("cannot create appointment with end Date before of start date", () => {
+  const startsAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() - 1);
+
+  expect(() => {
+    return new Appointment({
+      costumer: "Jon Doe",
+      startsAt,
+      endsAt,
+    });
+  }).toThrow();
 });
